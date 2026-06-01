0341a7fa6799565dbc2873b38332c29efee6002b
Jos De Graeve <jos.degraeve@accomodata.be>
2026-06-01 16:31:35 +0200

    WIP

--- click_odoo_contrib/backupdb.py
+++ click_odoo_contrib/backupdb.py
@@ -11,7 +11,9 @@ import fsspec
 # Monkey patch s3fs to fix InvalidPartOrder during concurrent uploads
 try:
     import s3fs.core
+
     _original_s3fs_commit = s3fs.core.S3File.commit
+
     def _patched_s3fs_commit(self):
         if hasattr(self, "parts") and isinstance(self.parts, list):
             try:
@@ -19,6 +21,7 @@ try:
             except Exception:
                 pass
         return _original_s3fs_commit(self)
+
     s3fs.core.S3File.commit = _patched_s3fs_commit
 except ImportError:
     pass
@@ -30,56 +33,25 @@ from click_odoo import odoo
 from ._backup import backup
 from ._dbutils import db_exists, db_management_enabled, pg_connect
 
-# Layout for files & folders inside the ZIP archive:
-MANIFEST_FILENAME = "manifest.json"
-DBDUMP_FILENAME = "db.dump"
-FILESTORE_DIRNAME = "filestore"
-FS_ATTACHMENT_DIRNAME = "fs_attachment"
+from ._storage_config import (
+    MANIFEST_FILENAME,
+    DUMP_SQL_FILENAME,
+    DBDUMP_FILENAME,
+    FILESTORE_DIRNAME,
+    FS_ATTACHMENT_DIRNAME,
+    FS_STORAGE_BACKUP_ENTRY,
+    get_fsspec_filesystem,
+)
 
-# reading configration from odoo.conf:
-FS_STORAGE_KEY= 'fs_storage.'
-FS_STORAGE_BACKUP_ENTRY = 'odoo_backups'
-
-fsspec_filesystems = {}
 
 # default chunk size for upload with streaming backup
 FS_WRITE_CHUNK_SIZE = 4 * 1024 * 1024
 
 
-def _read_fs_storage_config():
-    for item, vals in odoo.tools.config.misc.items():
-        if not item.startswith(FS_STORAGE_KEY):
-            continue
-        _, storage_code = item.split(".")
-        try:
-            protocol = vals["protocol"]
-            options = json.loads(vals["options"])
-            directory_path = vals["directory_path"]
-        except Exception as ex:
-            print(
-                "Failed to read fs_storage config, "
-                'expecting keys "protocol", "options" and "directory_path"'
-            )
-            raise Exception from ex
-        fs = fsspec.filesystem(protocol, **options)
-
-        fsspec_filesystems.update(
-            {
-                storage_code: (fs, directory_path),
-            }
-        )
-
-
-def _get_fsspec_filesystem(storage_code):
-    if storage_code not in fsspec_filesystems:
-        raise Exception("Storage code %s not configured in odoo config", storage_code)
-    return fsspec_filesystems[storage_code]
-
-
 def _dump_db_command(dbname, backup):
     cmd = ["pg_dump", "--no-owner", dbname]
     env = odoo.tools.misc.exec_pg_environ()
-    filename = "dump.sql"
+    filename = DUMP_SQL_FILENAME
     if backup.format in {"dump", "folder"}:
         cmd.insert(-1, "--format=c")
         filename = DBDUMP_FILENAME
@@ -88,7 +60,7 @@ def _dump_db_command(dbname, backup):
 
 def _dump_db(dbname, backup):
     cmd, env, filename = _dump_db_command(dbname, backup)
-    if backup.format in ("stream-zip","fsspec-zip"):
+    if backup.format in ("stream-zip", "fsspec-zip"):
         backup.add_dump_command(cmd, env, filename)
     else:
         stdout = subprocess.Popen(
@@ -143,7 +115,7 @@ def _backup_filestore(cr, dbname, backup, minimal):
                 storage_code,
                 fname,
             )
-            fs, directory_path = _get_fsspec_filesystem(storage_code)
+            fs, directory_path = get_fsspec_filesystem(storage_code)
             path = os.path.normpath(os.path.join(directory_path, fname))
             with fs.open(path, mode="rb") as fh:
                 backup.add_fileh(fh, archive_name)
@@ -197,7 +169,6 @@ def _backup_filestore(cr, dbname, backup, minimal):
     flag_value="minimal",
     help="Include only minimal filestore",
 )
-
 @click.argument("dbname", nargs=1)
 @click.argument("dest", nargs=1, required=1)
 def main(
@@ -228,7 +199,7 @@ def main(
     Finally this script allows to upload directly to remote storage
     streaming the zip file without having memory or diskspace
     constraints that large backups might introduce.  Choose stream-zip
-    as output format for this. Configuration for fsspec is 
+    as output format for this. Configuration for fsspec is
     read from odoo.conf
 
     This script also supports backup from attachments stored using
@@ -256,13 +227,10 @@ def main(
             else:
                 shutil.rmtree(dest)
 
-    _read_fs_storage_config()
+    if format in ("fsspec-zip", "stream-zip"):
+        fs, directory = get_fsspec_filesystem(FS_STORAGE_BACKUP_ENTRY)
 
-    if format in("fsspec-zip", "stream-zip"):
-
-        fs, directory = _get_fsspec_filesystem(FS_STORAGE_BACKUP_ENTRY)
-
-        backup_fullpath = f'{directory}/{dest}'
+        backup_fullpath = f"{directory}/{dest}"
         # Run touch to identify access problems early
         fs.touch(backup_fullpath)
 
@@ -272,8 +240,8 @@ def main(
     try:
         if format == "fsspec-zip":
             fsspec_out = fs.open(
-                backup_fullpath, 
-                mode='wb', 
+                backup_fullpath,
+                mode="wb",
                 blocksize=FS_WRITE_CHUNK_SIZE,
             )
         else:
@@ -282,26 +250,23 @@ def main(
         with backup(
             format, dest, "w", fsspec_out=fsspec_out
         ) as _backup, db.cursor() as cr, db_management_enabled():
-
             if format != "dump":
-                print('Adding manifest')
+                print("Adding manifest")
                 _create_manifest(cr, dbname, _backup)
             if filestore == "minimal":
-                print('Adding minimal filestore')
+                print("Adding minimal filestore")
                 _backup_filestore(cr, dbname, _backup, minimal=True)
             elif filestore == "full":
-                print('Adding full filestore')
+                print("Adding full filestore")
                 _backup_filestore(cr, dbname, _backup, minimal=False)
 
-            print('Adding dump')
+            print("Adding dump")
             _dump_db(dbname, _backup)
         if format == "stream-zip":
             backup_fileh = _backup.get_file_object()
 
             with fs.open(
-                backup_fullpath, 
-                mode='wb', 
-                blocksize=FS_WRITE_CHUNK_SIZE
+                backup_fullpath, mode="wb", blocksize=FS_WRITE_CHUNK_SIZE
             ) as upload_fileh:
                 while True:
                     buffer = backup_fileh.read(FS_WRITE_CHUNK_SIZE)
